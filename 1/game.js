fcde1944cdad8a85dce9078860eb827178f29923
Keith Simmons <keith@the-simmons.net>
2016-08-15 23:13:40 -0700

    page rendering

--- 1/game.js
+++ 1/game.js
@@ -1,6 +1,6 @@
 var Game1 = (function () {
     function Game1() {
-        this.game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', {
             preload: this.preload,
             create: this.create,
             update: this.update
@@ -8,12 +8,22 @@ var Game1 = (function () {
     }
     Game1.prototype.preload = function () {
         this.game.load.image('player', 'assets/Player.png');
+        this.game.load.image('wall', 'assets/Wall.png');
     };
     Game1.prototype.create = function () {
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        this.game.add.sprite(0, 0, 'player');
+        this.player = this.game.add.sprite(0, 0, 'player');
+        this.player.anchor.setTo(0.5, 0.5);
+        this.player.rotation = 90;
+        this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
+        this.player.body.allowRotation = true;
+        this.game.add.sprite(0, 100, 'wall');
     };
     Game1.prototype.update = function () {
+        this.player.rotation = this.game.physics.arcade.moveToPointer(this.player, 60, this.game.input.activePointer, 500);
     };
     return Game1;
 }());
+window.onload = function () {
+    var game = new Game1();
+};
--- 1/game.ts
+++ 1/game.ts
@@ -3,7 +3,7 @@
 
 class Game1 {
     constructor() {
-        this.game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', {
             preload: this.preload,
             create: this.create,
             update: this.update
@@ -11,17 +11,31 @@ class Game1 {
     }
 
     game: Phaser.Game;
+    player: Phaser.Sprite;
 
     preload() {
         this.game.load.image('player', 'assets/Player.png');
+        this.game.load.image('wall', 'assets/Wall.png');
     }
 
     create() {
         this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        this.game.add.sprite(0, 0, 'player');
+
+        this.player = this.game.add.sprite(0, 0, 'player');
+        this.player.anchor.setTo(0.5, 0.5);
+        this.player.rotation = 90;
+
+        this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
+        this.player.body.allowRotation = true;
+
+        this.game.add.sprite(0, 100, 'wall');
     }
 
     update() {
-
+        this.player.rotation = this.game.physics.arcade.moveToPointer(this.player, 60, this.game.input.activePointer, 500);
     }
+}
+
+window.onload = () => {
+    var game = new Game1();
 }
