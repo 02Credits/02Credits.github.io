f62e7893e0cb9ccf3bbd9b1222642128528f7d29
keith <keith@the-simmons.net>
2019-10-19 01:04:25 -0700

    day 85

--- blog/day47-garbage-rendering/updateTrees.ps1
+++
@@ -1,1 +0,0 @@
-pando todo.pando; cp todo.pando ./Site/content/projects/ta/todo.pando -Force; ((Get-Content -path ./Site/content/projects/ta/todo.pando -Raw) -replace '>' 'x') | Set-Content -Path ./Site/content/projects/ta/todo.pando; pando ./Site/content/projects/ta/todo.pando
